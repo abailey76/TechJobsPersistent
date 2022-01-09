65664e809d8f40c7820e67d0e956017c126425aa
Adam Bailey <abailey76@duck.com>
2022-01-09 17:58:43 -0600

    form validation, maybe

--- TechJobsPersistent/ViewModels/AddJobViewModel.cs
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using TechJobsPersistent.Models;
 
 namespace TechJobsPersistent.ViewModels
